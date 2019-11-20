1afdac879f8a6310a88bd3b5b95e18a5bc53e6fa
Radek Baczynski <radek.baczynski@gmail.com>
2019-11-20 13:37:23 +0100

    api docs

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 echo "Hello, I'm generating docs for: ${1} to output directory: ${2}"
 
-node /asyncapi/cli -t /generator/templates -o /generated "${1}" "${2}"
+node /asyncapi/cli -t /generator/templates -o "${2}" "${1}" html
